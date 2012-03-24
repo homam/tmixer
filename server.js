30b7fd23008f7a925b02bdf4331b1a275398ccb4
Homam <bluesnowball@gmail.com>
2012-03-25 00:56:31 +0400

    initial push

---
+++ server.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="views/public/scripts/libs/_.js" />
+
+var express = require('express');
+
+var port = process.env.PORT || parseInt(process.argv.pop());
+
+var app = express.createServer();
+
+app.configure(function () {
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.use('/js', express.static(__dirname + '/views/js'));
+    app.use('/img', express.static(__dirname + '/views/img'));
+    app.use('/audio', express.static(__dirname + '/views/audio'));
+    app.use('/css', express.static(__dirname + '/views/css'));
+    app.use('/impulse-responses', express.static(__dirname + '/views/impulse-responses'));
+
+});
+
+app.set('view engine', 'html');
+app.register('.html', require('jqtpl').express);
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        layout: false
+    });
+});
+app.get('/homam', function (req, res) {
+    res.end('hhahah!');
+});
+
+
+// how to read a file: fs.readFile('socket.client.js', 'utf-8', function (a, d) { res.end(d); })
+
+app.listen(port);;
+
---
+++ views/js/channel.js
@@ -0,0 +1,37 @@
+﻿Channel = function Channel(autoPlay, loop, src) {
+    this.audio = new Audio();
+    this.playing = false;
+    var self = this;
+    this.audio.autoplay = !!autoPlay;
+    this.audio.loop = !!loop;
+    if (!!src)
+        this.audio.src = src;
+    this.audio.addEventListener('ended', function () {
+        self.playing = false;
+    }, false);
+};
+
+Channel.prototype.play = function Channel$start(src) {
+    if (this.playing) {
+        return false;
+    } else {
+        this.playing = true;
+        if (!!src)
+            this.audio.src = src;
+    }
+};
+
+Channel.prototype.stop = function Channel$stop() {
+    this.audio.pause();
+    this.playing = false;
+};
+
+Channel.prototype.toggle = function Channel$toggle() {
+    if (this.playing) {
+        this.stop();
+    }
+    else {
+        this.audio.currentTime = 0;
+        this.play();
+    }
+};
